0afd5158d44d4bd47a0cf12b43d738b453f8c0ad
asos-olivier <olivier.bonal@asos.com>
2017-11-30 11:23:19 +0000

    Creates common module + start testing Authentication

--- Android-AsosCoffee/mobile/src/main/java/com/asos/coffee/MainActivity.kt
+++ Android-AsosCoffee/mobile/src/main/java/com/asos/coffee/MainActivity.kt
@@ -5,8 +5,18 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.ResultCodes
+import com.firebase.ui.auth.IdpResponse
+import android.content.Intent
+import android.util.Log
+
+
+private const val TAG = "ASOS-COVFEFE-CLIENT"
+private const val RC_SIGN_IN = 123
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +24,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        // Choose authentication providers
+        val providers = listOf(
+                AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -34,6 +58,24 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == ResultCodes.OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Log.i(TAG, "User $user logged in! ")
+
+            } else {
+                // Sign in failed, check response for error code
+                Log.e(TAG, "Log In Failure ")
+            }
         }
     }
 }
