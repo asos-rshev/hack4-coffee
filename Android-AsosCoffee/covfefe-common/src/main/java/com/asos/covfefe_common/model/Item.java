ff76b2403cc0dcd387085773b7f428a85be4658e
krystian.plonski <krystian.plonski@asos.com>
2017-11-30 19:28:58 +0000

    initial commit of CovfefeShop App

---
+++ Android-AsosCoffee/covfefe-common/src/main/java/com/asos/covfefe_common/model/Item.java
@@ -0,0 +1,22 @@
+package com.asos.covfefe_common.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Item implements Serializable {
+    public String count;
+
+    public String name;
+
+    public String unitPrice;
+
+    public String type;
+
+    public String milky;
+
+    public String ready;
+
+    public List<String> extras;
+
+    public String size;
+}
