e43c50157c0f41f7711568789575bc65455a630e
Roman Shevtsov <roman.shevtsov@gmail.com>
2017-11-30 10:52:21 +0000

    Adds iOS project.

---
+++ iOS-Covfefe/Covfefe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Covfefe
+//
+//  Created by Roman Shevtsov on 30/11/2017.
+//  Copyright © 2017 ASOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ iOS-Covfefe/CovfefeTests/CovfefeTests.swift
@@ -0,0 +1,36 @@
+//
+//  CovfefeTests.swift
+//  CovfefeTests
+//
+//  Created by Roman Shevtsov on 30/11/2017.
+//  Copyright © 2017 ASOS. All rights reserved.
+//
+
+import XCTest
+@testable import Covfefe
+
+class CovfefeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
