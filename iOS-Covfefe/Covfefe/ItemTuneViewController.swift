0111bfb2752380d8e121ce6a2aca06a8c370018a
Roman Shevtsov <roman.shevtsov@asos.com>
2017-12-01 11:17:29 +0000

    Adds ItemTuneVC with empty sizes collectionview.

--- iOS-Covfefe/Covfefe/ItemTuneViewController.swift
+++ iOS-Covfefe/Covfefe/ItemTuneViewController.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
-class ItemTuneViewController: UIViewController {
+class ItemTuneViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     var item: Item!
+    lazy var variant: NameAndPrice? = {
+        return self.item.sizes.first
+    }()
+
+    @IBOutlet var heroImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var variantLabel: UILabel!
+    @IBOutlet var proceedButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = item.name
+        updateVariant()
+
     }
+
+    func updateVariant() {
+        variantLabel.text = variant?.name
+
+        let proceedTitle = "PROCEED (total: £\(variant?.price ?? 999))"
+        proceedButton.setTitle(proceedTitle, for: .normal)
+    }
+
+    @IBAction func didTapProceedButton() {
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return item.sizes.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemSizeCollectionViewCell", for: indexPath) as! ItemSizeCollectionViewCell
+        return cell
+    }
+
+
 }
