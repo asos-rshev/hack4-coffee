80bd06f72f8b349478ac409b88164e5cf75c0777
asos-olivier <olivier.bonal@asos.com>
2017-12-01 11:48:06 +0000

    Add extras

--- Android-AsosCoffee/mobile/src/main/java/com/asos/coffee/CustomiseActivity.kt
+++ Android-AsosCoffee/mobile/src/main/java/com/asos/coffee/CustomiseActivity.kt
@@ -11,8 +11,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.asos.covfefe_common.mapper.CanteenItemSizeNameToIconMapper
 import com.asos.covfefe_common.model.CanteenMenuItem
+import com.asos.covfefe_common.model.CanteenMenuItemExtra
 import com.asos.covfefe_common.model.CanteenMenuItemSize
 import kotlinx.android.synthetic.main.activity_customise.*
+import kotlinx.android.synthetic.main.extras_item.view.*
 import kotlinx.android.synthetic.main.size_item.view.*
 
 private const val EXTRA_ITEM: String = "EXTRA_ITEM"
@@ -24,6 +26,10 @@ class CustomiseActivity : AppCompatActivity() {
         result
     }
 
+    private var selectedSize: CanteenMenuItemSize? = null
+    private var selectedExtra: CanteenMenuItemExtra? = null
+//    private var selectedMilk: CanteenMenuItemMilk? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customise)
@@ -34,18 +40,36 @@ class CustomiseActivity : AppCompatActivity() {
         milkRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
         val sizesAdapter = SizesAdapter(item.sizes ?: emptyList()) {
-            updateFromSelection(it)
+            selectedSize = it
+            updateFromSelection()
         }
-        item.sizes?.get(0)?.let { sizesAdapter.selectedSize = it }
+        item.sizes?.get(0)?.let {
+            selectedSize = it
+            sizesAdapter.selectedSize = selectedSize
+        }
         sizesRecyclerView.adapter = sizesAdapter
 
-        updateFromSelection(sizesAdapter.selectedSize)
+        if (item.milky) {
+            // TODO: MilkAdapter
+        }
+
+        val extrasAdapter = ExtrasAdapter(item.extras ?: emptyList()) {
+            selectedExtra = it
+            updateFromSelection()
+        }
+        extrasAdapter.selectedExtra = null
+        extrasRecyclerView.adapter = extrasAdapter
+
+        updateFromSelection()
     }
 
-    private fun updateFromSelection(selectedSize: CanteenMenuItemSize?) {
-        customiseProceedButton.text = getString(R.string.proceed_button_label_format, selectedSize?.price ?: 0.0)
-        customizeSummary.text = selectedSize?.name
-        sizesRecyclerView.adapter.notifyDataSetChanged()
+    private fun updateFromSelection() {
+        val sizePrice: Double = selectedSize?.price ?: 0.0
+        val extraPrice: Double = selectedExtra?.price ?: 0.0
+        customiseProceedButton.text = getString(R.string.proceed_button_label_format, sizePrice + extraPrice)
+        val summary = StringBuilder(selectedSize?.name)
+        selectedExtra?.let { summary.append(", ${it.name}") }
+        customizeSummary.text = summary.toString().toLowerCase()
     }
 
     companion object {
@@ -77,7 +101,28 @@ class SizesAdapter(val items: List<CanteenMenuItemSize>, val buttonUpdater: (Can
             notifyDataSetChanged()
         })
     }
+}
 
+class ExtrasAdapter(private val items: List<CanteenMenuItemExtra>, private val buttonUpdater: (CanteenMenuItemExtra?) -> Unit) : RecyclerView.Adapter<ExtrasViewHolder>() {
+
+    var selectedExtra: CanteenMenuItemExtra? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExtrasViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.extras_item, parent, false)
+        return ExtrasViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ExtrasViewHolder?, position: Int) {
+        val extra = items[position]
+        holder?.bind(extra, selectedExtra == extra, View.OnClickListener {
+            selectedExtra = extra
+            buttonUpdater.invoke(extra)
+            notifyDataSetChanged()
+        })
+    }
 }
 
 class SizesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -87,7 +132,28 @@ class SizesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         itemView.itemSizeIcon.setImageResource(canteenItemSizeNameToIconMapper.iconForSize(size.name))
         itemView.itemSizeTitle.text = size.name
         itemView.itemSizePrice.text = "£${size.price}"
-        itemView.customiseSelectedBg?.visibility = if (selected) View.VISIBLE else View.GONE
+        itemView.itemSizeSelectedBg?.visibility = if (selected) View.VISIBLE else View.GONE
         itemView.setOnClickListener(clickAction)
     }
 }
+
+class ExtrasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(extra: CanteenMenuItemExtra, selected: Boolean, clickAction: View.OnClickListener) {
+        itemView.itemExtraTitle.text = extra.name
+        itemView.itemExtraPrice.text = "£${extra.price}"
+        itemView.itemExtraSelectedBg?.visibility = if (selected) View.VISIBLE else View.GONE
+        itemView.setOnClickListener(clickAction)
+    }
+}
+
+//class MilkViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//    private val canteenItemSizeNameToIconMapper = CanteenItemSizeNameToIconMapper()
+//
+//    fun bind(milk: CanteenMenuItemMilk, selected: Boolean, clickAction: View.OnClickListener) {
+//        itemView.itemMilkIcon.setImageResource(canteenItemSizeNameToIconMapper.iconForSize(milk.name))
+//        itemView.itemMilkTitle.text = milk.name
+//        itemView.itemMilkPrice.text = "£${milk.price}"
+//        itemView.itemMilkSelectedBg?.visibility = if (selected) View.VISIBLE else View.GONE
+//        itemView.setOnClickListener(clickAction)
+//    }
+//}
